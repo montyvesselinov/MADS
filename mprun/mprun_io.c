8eb507773e33ad0bfc543620d89b28f8ad98a69c
monty <velimir.vesselinov@gmail.com>
2014-04-11 15:20:47 -0600

    mprun changes

--- mprun/mprun_io.c
+++ mprun/mprun_io.c
@@ -46,10 +46,10 @@ int create_mprun_dir( char *dir )
 	int r;
 	sprintf( buf, "%s \"mkdir ../%s >& /dev/null\"", SHELL, dir );
 	r = system( buf );
-	if( r == -1 || r == 127 ) {	 tprintf( "ERROR: System call failed!\n" ); return( -1 ); }
+	if( r == -1 || r == 127 ) {	tprintf( "ERROR: System call to created dir failed!\n" ); return( -1 ); }
 	sprintf( buf, "%s \"ln -s $PWD/* ../%s >& /dev/null\"", SHELL, dir );
 	r = system( buf );
-	if( r == -1 || r == 127 ) {	 tprintf( "ERROR: System call failed!\n" ); return( -1 ); }
+	if( r == -1 || r == 127 ) {	tprintf( "ERROR: System call to link files failed!\n" ); return( -1 ); }
 	return( 0 );
 }
 
@@ -59,7 +59,7 @@ int delete_mprun_dir( char *dirs )
 	int r;
 	sprintf( buf, "%s \"rm -fR ../%s >& /dev/null\"", SHELL, dirs );
 	r = system( buf );
-	if( r == -1 || r == 127 ) {	 tprintf( "ERROR: System call failed!\n" ); return( -1 ); }
+	if( r == -1 || r == 127 ) {	tprintf( "ERROR: System call to delete dir failed!\n" ); return( -1 ); }
 	return( 0 );
 }
 
@@ -71,10 +71,10 @@ int create_mprun_dirs( int nDir, char **dirs )
 	{
 		sprintf( buf, "%s \"mkdir ../%s  >& /dev/null\"", SHELL, dirs[i] );
 		r = system( buf );
-		if( r == -1 || r == 127 ) {	 tprintf( "ERROR: System call failed!\n" ); return( -1 ); }
+		if( r == -1 || r == 127 ) {	tprintf( "ERROR: System call create dirs failed!\n" ); return( -1 ); }
 		sprintf( buf, "%s \"ln -s $cwd/* ../%s\"", SHELL, dirs[i] );
 		r = system( buf );
-		if( r == -1 || r == 127 ) {	 tprintf( "ERROR: System call failed!\n" ); return( -1 ); }
+		if( r == -1 || r == 127 ) {	tprintf( "ERROR: System call to link files failed!\n" ); return( -1 ); }
 	}
 	return( 0 );
 }
@@ -87,7 +87,7 @@ int delete_mprun_dirs( int nDir, char **dirs )
 	{
 		sprintf( buf, "%s \"rm -fR ../%s >& /dev/null\"", SHELL, dirs[i] );
 		r = system( buf );
-		if( r == -1 || r == 127 ) {	 tprintf( "ERROR: System call failed!\n" ); return( -1 ); }
+		if( r == -1 || r == 127 ) {	tprintf( "ERROR: System call to delete dirs failed!\n" ); return( -1 ); }
 	}
 	return( 0 );
 }
