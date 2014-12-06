525854886594c6fcf5320954fb9092a99d988858
monty <velimir.vesselinov@gmail.com>
2014-12-06 14:26:10 -0700

    BuildLibYAML.cmake added

---
+++ tpls/cmake/BuildLibYAML.cmake
@@ -0,0 +1,12 @@
+#  -*- mode: cmake -*-
+message(STATUS "Installing LibYAML (${LIBYAML_VERSION})")
+ExternalProject_Add(
+	libyaml
+	DOWNLOAD_DIR ${TPL_DOWNLOAD_DIR}
+	URL          ${LIBYAML_URL}
+	CONFIGURE_COMMAND ./configure CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} --prefix=${TPL_INSTALL_PREFIX} WORKING_DIRECTORY ${SOURCE_DIR}
+	BUILD_COMMAND     make
+	BUILD_IN_SOURCE   1
+	INSTALL_DIR      ${TPL_INSTALL_PREFIX}
+	INSTALL_COMMAND  make install
+)
