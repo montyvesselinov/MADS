b4eb917c6b923794c00873cafdcd8cf131fb9992
monty <velimir.vesselinov@gmail.com>
2015-05-06 20:02:59 -0600

    source ~/.bashrc added

--- src/mprun/mprun.c
+++ src/mprun/mprun.c
@@ -409,7 +409,7 @@ int mprunall( int nJob, void *data, double *var_mat[], double *phi, double *f[] 
 						execlp( "bpsh", "bpsh", kidhost[child], "/usr/bin/env", "bash", "-c", buf, ( char * ) 0 );
 						break;
 					case SRUN:
-						sprintf( buf, "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/projects/hpcsoft/toss2/common/gcc/4.9.2/lib64; cd %s; %s", dir, exec_name );
+						sprintf( buf, "source ~/.bashrc; cd %s; %s", dir, exec_name );
 						execlp( "srun", "srun", "--exclusive", "-N1", "-n1", cpu_per_task, "/usr/bin/env", "bash", "-c", buf, ( char * ) 0 );
 						break;
 					case SHELL:
@@ -844,7 +844,7 @@ int mprun( int nJob, void *data )
 						execlp( "bpsh", "bpsh", kidhost[child], "/usr/bin/env", "bash", "-c", buf, ( char * ) 0 );
 						break;
 					case SRUN:
-						sprintf( buf, "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/projects/hpcsoft/toss2/common/gcc/4.9.2/lib64; cd %s; %s", dir, exec_name );
+						sprintf( buf, "source ~/.bashrc; cd %s; %s", dir, exec_name );
 						execlp( "srun", "srun", "--exclusive", "-N1", "-n1", cpu_per_task, "/usr/bin/env", "bash", "-c", buf, ( char * ) 0 );
 						break;
 					case SHELL:
